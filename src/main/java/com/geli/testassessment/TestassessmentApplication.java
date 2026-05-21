c813993d6ca0b52d228feaac28533325b9f1bf96
Irfan Naufal Ridi <naufalirfann@gmail.com>
2026-05-21 18:00:33 +0700

    init project

---
+++ src/main/java/com/geli/testassessment/TestassessmentApplication.java
@@ -0,0 +1,13 @@
+package com.geli.testassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestassessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestassessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geli/testassessment/TestassessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geli.testassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestassessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
