bdce61ecdb4f36c2d9f53b35e8a42db93de807df
Irfan Naufal Ridi <naufalirfann@gmail.com>
2026-05-22 09:44:38 +0700

    make update item

--- src/main/java/com/geli/testassessment/repository/ItemRepository.java
+++ src/main/java/com/geli/testassessment/repository/ItemRepository.java
@@ -1,5 +1,7 @@
 package com.geli.testassessment.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.geli.testassessment.model.entity.Item;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    
+    Optional<Item> findByItemNameIgnoreCase(String itemName);
 }
